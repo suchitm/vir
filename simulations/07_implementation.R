a6c0412ca8a25bf7e4786406a18e5591ab2f25c4
Suchit Mehrotra <suchitmehrotra@gmail.com>
2020-08-22 15:50:21 -0500

    starting applied analysis script

--- simulations/07_implementation.R
+++ simulations/07_implementation.R
@@ -3,6 +3,11 @@ library(vir)
 # generate data
 X = matrix(nrow = 1000, ncol = 100, rnorm(100 * 1000))
 b = rnorm(100)
-y = rnorm(1) + X %*% b + rnorm(1000, sd = 0.2)
+b[21:100] = 0
+y = rnorm(1) + X %*% b + rnorm(1000)
 
-cavi_fit = lm_ridge_cavi(y, X, n_iter = 100)
+ridge_cavi_fit = lm_ridge_cavi(y, X, n_iter = 100)
+hs_cavi_fit = lm_hs_cavi(y, X, n_iter = 100)
+
+mean((b - hs_cavi_fit$mu_b)^2)
+mean((b - ridge_cavi_fit$mu_b)^2)
