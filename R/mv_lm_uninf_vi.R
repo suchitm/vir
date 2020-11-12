3fe6b373f8755fd2f10159d93c8ad9a228be9911
Suchit Mehrotra <SuchitMehrotra@savvysherpa.com>
2020-11-12 12:26:06 -0600

    predict vi function

--- R/mv_lm_uninf_vi.R
+++ R/mv_lm_uninf_vi.R
@@ -1,44 +1,52 @@
-get_params_vi = function(mvlm_fit, M, P, K)
+#' generate predictions for a multivariate fit from VI algorithms
+#' @param fit model fit
+#' @param X_test matrix of predictor values at which to predict the response
+#' @param n_samps number of samples to draw from the variational predictive
+#'   distribution
+#' @return Array of draws from the predictive distribution, along with the means
+#'   of the distributions, and their lower (2.5) and upper (97.5) bounds of the
+#'   credible intervals.
+#' @export
+predict_mv_lm_vi = function(fit, X_test, n_samps)
 {
-  # turn theta var-cov into array
-  var_theta_array = array(dim = c(M, K, K), data = NA)
-  for(m in 1:M)
+  new_N = nrow(X_test)
+  one_N = rep(1, new_N)
+  P = ncol(X_test)
+  M = nrow(fit$theta$mu)
+  K = ncol(fit$theta$mu)
+
+  msigma_theta_chol = chol(fit$theta$msigma)
+  msigma_B_chol = chol(fit$B$msigma)
+
+  Y_array = array(dim = c(n_samps, new_N, M), NA)
+
+  for(i in 1:n_samps)
   {
-    start = (m - 1) * K + 1
-    end = start + K - 1
-    var_theta_array[m, , ] = mvlm_fit$msigma_mat_theta[start:end, ]
+    # draw values
+    b0 = fit$b0$mu + rnorm(M) * sqrt(fit$b0$vsigma2)
+    B = fit$B$mu + matrix(nrow = M, ncol = P, rnorm(M * P)) %*% msigma_B_chol
+    theta = fit$theta$mu +
+      matrix(nrow = M, ncol = K, rnorm(M * K)) %*% msigma_theta_chol
+    tau = rgamma(n = 1, shape = fit$tau$shape, rate = fit$tau$rate)
+
+    # draw sample
+    Y_array[i, , ] = one_N %*% t(b0) + X_test %*% t(B) +
+      matrix(nrow = new_N, ncol = K, rnorm(new_N * K)) %*% t(theta) +
+      1 / sqrt(tau) * matrix(nrow = new_N, ncol = M, rnorm(new_N * M))
   }
 
-  # turn B var-cov into array
-  var_b_array = array(dim = c(M, P, P), data = NA)
-  for(m in 1:M)
-  {
-    start = (m - 1) * P + 1
-    end = start + P - 1
-    var_b_array[m, , ] = mvlm_fit$msigma_mat_b[start:end, ]
-  }
+  means = apply(Y_array, MARGIN = c(2, 3), FUN = mean)
+  lower = apply(
+    Y_array, MARGIN = c(2, 3), FUN = function(x) quantile(x, probs = 0.025)
+  )
+  upper = apply(
+    Y_array, MARGIN = c(2, 3), FUN = function(x) quantile(x, probs = 0.975)
+  )
 
-  b0 = vector("list")
-  b0$dist = 'multivariate normal - independent'
-  b0$mu = mvlm_fit$vmu_b0
-  b0$vsigma2 = mvlm_fit$vsigma2_b0
-
-  B = vector("list")
-  B$dist = 'matrix normal - independent over rows'
-  B$mu = mvlm_fit$vmu_b
-  B$msigma_array = var_b_array
-
-  theta = vector("list")
-  theta$dist = 'matrix normal - independent over rows'
-  theta$mu = mvlm_fit$vmu_theta
-  theta$msigma_array = var_theta_array
-
-  tau = vector("list")
-  tau$dist = 'independent gamma'
-  tau$shape = mvlm_fit$param_tau$shape
-  tau$rate = mvlm_fit$param_tau$rate
-
-  retl = list(b0 = b0, B = B, theta = theta, tau = tau)
-
-  return(retl)
+  return(
+    samples = Y_array,
+    mean = means,
+    lower_bound = lower,
+    upper_bound = upper
+  )
 }
